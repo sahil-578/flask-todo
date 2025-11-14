476341596867028a1712def5e62b59a11f4152cf
Sahil <sahil1372001@gmail.com>
2025-11-15 00:51:23 +0530

    Recommit state with only Item ID

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGODB_URI")
+client = MongoClient("MONGO_URI")
+db = client.todo
 
 app = Flask(__name__)
 
@@ -12,6 +21,17 @@ def api():
         "message" : "Initial JSON data",
         "version" : 2}    
     return jsonify(data)
+
+@app.route("/submittodoitem", methods=["POST"])
+def submit_item():
+    item = {
+        "itemName": request.form.get("itemName"),
+        "itemDescription": request.form.get("itemDescription")
+    }
+    db.items.insert_one(item)
+    return "Saved!"
+
+#try
 
 if __name__ == "__main__":
     app.run(debug= True) 
